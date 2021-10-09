4b234a542ace0afd7a2ff1e7b1fcf387e527718b
qwanysh <olimpsdu@gmail.com>
2021-10-10 02:33:36 +0600

    Cache shortenings

--- backend/src/shortenings/utils.py
+++ backend/src/shortenings/utils.py
@@ -1,5 +1,11 @@
 import random
 import string
+from typing import Optional
+
+from rest_framework.request import Request
+
+from core.redis_client import redis_client
+from shortenings import consts
 
 
 def get_random_string(length: int) -> str:
@@ -7,5 +13,14 @@ def get_random_string(length: int) -> str:
     return ''.join(random.choices(alphabet, k=length))
 
 
-def get_app_base_address(request):
+def get_app_base_address(request: Request) -> str:
     return f'{request.scheme}://{request.get_host()}'
+
+
+def cache_shortening(slug: str, target_url: str):
+    redis_client.setex(slug, consts.SHORTENING_CACHE_TTL, target_url)
+
+
+def get_cached_target_url(slug: str) -> Optional[str]:
+    if target_url := redis_client.get(slug):
+        return target_url.decode()
--- backend/src/shortenings/views.py
+++ backend/src/shortenings/views.py
@@ -1,13 +1,20 @@
 from django.http import Http404
 from django.views.generic import RedirectView
 
+from shortenings import utils
 from shortenings.models import Shortening
 
 
 class ShorteningRedirectView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
+        slug = kwargs.get('slug')
+
+        if cached_target_url := utils.get_cached_target_url(slug):
+            return cached_target_url
+
         try:
-            shortening = Shortening.objects.get(slug=kwargs.get('slug'))
+            shortening = Shortening.objects.get(slug=slug)
+            utils.cache_shortening(shortening.slug, shortening.target_url)
             return shortening.target_url
         except Shortening.DoesNotExist:
             raise Http404()
