1d5584f1d3c47a89d2e195cf8967072b1dbb58aa
qwanysh <olimpsdu@gmail.com>
2021-10-12 18:36:04 +0600

    Set shortening clean interval to 1 hour

--- backend/src/shortenings/tasks.py
+++ backend/src/shortenings/tasks.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 
 from core import celery_app
+from shortenings import consts
 from shortenings.models import Shortening
 
 
@@ -9,4 +10,6 @@ def delete_expired_shortenings():
     Shortening.objects.filter(expires_at__lt=timezone.now()).delete()
 
 
-celery_app.add_periodic_task(5, delete_expired_shortenings.s())
+celery_app.add_periodic_task(
+    consts.SHORTENING_CLEAN_INTERVAL, delete_expired_shortenings.s(),
+)
